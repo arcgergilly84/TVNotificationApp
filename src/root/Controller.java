14d3194ebcdd5aae489e5a1ad625ace1edfcc554
David <archergilly84@gmail.com>
2017-09-30 23:23:10 +0100

    Web Crawl function complete, append to file is incorrect.

--- src/root/Controller.java
+++ src/root/Controller.java
@@ -12,6 +12,8 @@ import javafx.scene.control.TextField;
 
 import java.io.*;
 
+import java.net.URL;
+import java.net.URLConnection;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -26,12 +28,14 @@ public class Controller {
     @FXML Label validate;
     @FXML ListView TVList;
     @FXML ListView currentview;
+    @FXML TextField code;
+    @FXML TextField urlEpisode;
 
 
     ObservableList<String> tvShows = FXCollections.observableArrayList();
     ObservableList<String> currentShows = FXCollections.observableArrayList();
     Map<Integer,ArrayList<String>> TVShows = new TreeMap<>();
-    SimpleDateFormat date = new SimpleDateFormat("dd/MM/yy");
+    SimpleDateFormat date = new SimpleDateFormat("dd MMM yy");
     String today = date.format(new Date());
     TV tv = null;
     int showID = 1;
@@ -39,12 +43,18 @@ public class Controller {
     public void addtoList(){
         String show = tvName.getText();
         String userDate = tvDate.getText();
+        String episodeCode = code.getText();
+        String episodeUrl = urlEpisode.getText();
         ArrayList<String> list = new ArrayList<>();
 
         tv = new TV(showID,show ,userDate);
+        tv.setCode(episodeCode);
+        tv.setEpisodeUrl(episodeUrl);
 
         list.add(tv.getname());
         list.add(tv.getDate());
+        list.add(tv.getCode());
+        list.add(tv.getEpisodeUrl());
 
         if(show.equals("")|| userDate.equals("")){
             validate.setText("Not null");
@@ -55,6 +65,7 @@ public class Controller {
         TVList.setItems(tvShows.sorted());
         showID += 1;
         clear();
+        System.out.println(TVShows);
     }
 
     public void exitApp(){
@@ -69,6 +80,8 @@ public class Controller {
     private void clear(){
         tvName.setText("");
         tvDate.setText("");
+        code.setText("");
+        urlEpisode.setText("");
     }
 
     private void persist() throws IOException{
@@ -91,11 +104,32 @@ public class Controller {
                 System.out.println("Broke");
             }
         }
+
         for (Map.Entry<Integer,ArrayList<String>> shows : TVShows.entrySet()){
             tvShows.add(shows.getValue().get(0));
             TVList.setItems(tvShows);
+            dynamicEpisode(shows.getValue().get(3), shows.getValue().get(2));
         }
+
         showCurrent();
+    }
+
+    public void dynamicEpisode(String epurl, String sCode)throws IOException{
+        URL url = new URL("http://epguides.com/" + epurl);
+        URLConnection uc = url.openConnection();
+        InputStream is = uc.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+        String line = null;
+
+        while ((line = br.readLine()) != null){
+            if (line.contains(sCode)){
+                line = line.substring(27, 36);
+                System.out.println(line);
+            }
+        }
+        is.close();
+        br.close();
     }
 
     public void showCurrent(){
--- src/root/TV.java
+++ src/root/TV.java
@@ -6,6 +6,8 @@ public class TV {
     private int showID = 0;
     private String name;
     private String date;
+    private String code;
+    private String episodeUrl;
 
     public TV(int ID, String name, String date){
         showID = ID;
@@ -22,5 +24,19 @@ public class TV {
     }
 
     public int getID(){return showID;}
+
+    public void setCode(String code){
+        this.code = code;
+    }
+
+    public String getCode(){return code;}
+
+    public String getEpisodeUrl() {
+        return episodeUrl;
+    }
+
+    public void setEpisodeUrl(String episodeUrl) {
+        this.episodeUrl = episodeUrl;
+    }
 
 }
